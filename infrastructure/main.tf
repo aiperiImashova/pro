ad4a8880110b6be7a1c968dfd3644baa058bcf4d
Administrator <gitlab_admin_2d51a5@example.com>
2024-09-21 05:54:49 +0000

    i

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,24 +1,24 @@
 # Создаем VPC
-# resource "google_compute_network" "vpc_network" {
-#   name = var.network_name
-#   auto_create_subnetworks = false
-# }
+resource "google_compute_network" "vpc_network" {
+  name = var.network_name
+  auto_create_subnetworks = false
+}
 
 # Создаем подсеть
-# resource "google_compute_subnetwork" "vpc_subnet" {
-#   name          = var.subnet_name
-#   # network       = google_compute_network.vpc_network.self_link
-#   ip_cidr_range = "10.0.0.0/16"
-#   region        = var.region
-# }
+resource "google_compute_subnetwork" "vpc_subnet" {
+  name          = var.subnet_name
+  # network       = google_compute_network.vpc_network.self_link
+  ip_cidr_range = "10.0.0.0/16"
+  region        = var.region
+}
 
 # Создаем GKE кластер
 resource "google_container_cluster" "primary_cluster" {
   name     = var.cluster_name
   location = var.region
 
-  # network    = google_compute_network.vpc_network.name
-  # subnetwork = google_compute_subnetwork.vpc_subnet.name
+  network    = google_compute_network.vpc_network.name
+  subnetwork = google_compute_subnetwork.vpc_subnet.name
 
   initial_node_count = 1  
   node_config {
